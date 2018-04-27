d184f30e354e734475afe142ae59e43891270ff3
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-04-27 19:51:08 +0200

    GreenTesty1\

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,20 @@
 public class StringCalculator {
 
+    private int result;
+
     public StringCalculator() {
     }
 
     public int add(String inputString) {
-        return -1;
-    }
+        if (inputString == "") return 0;
+        else {
+            String[] numbers = inputString.split("\\+");
+            for(String s : numbers){
+                int n = Integer.parseInt(s);
+                result += n;
+            }
+            return result;
+        }
+}
 
 }
