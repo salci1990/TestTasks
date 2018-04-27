8299d9552502d15542ee5513009693a3af531c4b
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-04-27 19:09:45 +0200

    Testy

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,10 @@
+public class StringCalculator {
+
+    public StringCalculator() {
+    }
+
+    public int add(String inputString) {
+        return -1;
+    }
+
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCalculator {
+
+   StringCalculator calc;
+
+    @Test
+    public void testEmptyString() {
+        int result = calc.add("");
+        assertEquals(0, result);
+    }
+    @Test
+    public void testOneNumber() {
+        int result = calc.add("7");
+        assertEquals(7, result);
+    }
+    @Test
+    public void testTwoNumber() {
+        int result = calc.add("3+5");
+        assertEquals(8, result);
+    }
+}
